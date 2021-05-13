5e348b828b738bbf87386e40fa74f65e28b962cd
DamianTrajster <74788379+DamianTrajster@users.noreply.github.com>
2021-05-12 23:21:16 -0300

    trabajo practico 3

---
+++ funcionesDeTareas.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const tarea = JSON.parse(fs.readFileSync('./data/tareas.json', 'utf-8'));
+
+
+const moduleTareas = {
+    listar: function() {
+        tarea.forEach((tarea,i) => {
+           console.log(`Tarea ${i+1} - ${tarea.titulo},${tarea.estado}`)
+        });
+    },
+    guardar: function(moduleTareas){ 
+    fs.writeFileSync('./data/tareas.json', JSON.stringify(tarea,null,2), 'utf-8');
+    },
+    crear: (titulo, estado = 'pendiente') => {
+        let = nuevaTarea = {
+            titulo,
+            estado
+        }
+       tarea.push(nuevaTarea)
+       moduleTareas.guardar()
+    },
+    filtrar: function(filtro){
+       let filtrar = tarea.filter(tarea => tarea.estado === filtro || tarea.titulo.includes(filtro));
+       console.log(filtrar)
+    }
+} 
+
+module.exports = moduleTareas;
